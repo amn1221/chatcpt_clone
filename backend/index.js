b3cde33c96de9b4707a692e684c87e0863e47fcd
Yesgat <aman@gmail.com>
2026-07-11 03:33:20 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,21 @@ import { errorHandler } from "./src/middleware/error-handler.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+ const allowedOrigins = process.env.ALLOWED_ORIGINS 
+  ? process.env.ALLOWED_ORIGINS.split(',') 
+  : ['http://localhost:5173'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use(express.json());
 
